b3776191c8344b8b2f29eb66ee8992a9a592c27c
张易 <zhangyi@zaicang.com>
2024-04-12 09:37:19 +0800

    提交

--- dist/dev/mp-weixin/enums/cacheEnum.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const TOKEN_KEY = "TOKEN__";
-exports.T = TOKEN_KEY;
--- dist/dev/mp-weixin/state/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const common_vendor = require("../common/vendor.js");
-const store = common_vendor.a();
-function setupStore(app) {
-  app.use(store);
-}
-exports.s = setupStore;
